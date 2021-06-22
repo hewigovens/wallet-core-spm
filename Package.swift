f32880f28b2de1d500b86db6f4bd1fb6329e546b
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-06-22 19:45:47 +0800

    revert to working example

--- Package.swift
+++ Package.swift
@@ -4,51 +4,24 @@ import PackageDescription
 
 let package = Package(
     name: "WalletCore",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_14),
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "WalletCore", type: .dynamic, targets: ["WalletCore", "SwiftProtobuf", "WalletCoreCommon"]
+            name: "WalletCore", targets: ["WalletCore", "SwiftProtobuf"]
         )
     ],
     dependencies: [
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "WalletCore",
-            dependencies: ["SwiftProtobuf", "WalletCoreCommon"],
-            path: "Sources",
-            exclude: [
-                "SecRandom.m",
-                "Generated/WalletCore.h"
-            ],
-            sources: [
-                "Generated/Enums",
-                "Generated/Protobuf",
-                "SecRandow.swift"
-            ],
-            cSettings: [
-                .headerSearchPath("include")
-            ],
-            cxxSettings: [
-                .headerSearchPath("include")
-            ],
-            linkerSettings: [
-                .linkedFramework("SwiftProtobuf"),
-                .linkedFramework("WalletCoreCommon")
-            ]
+            url: "https://github.com/hewigovens/wallet-core-spm/releases/download/0.0.1/WalletCore.xcframework.zip",
+            checksum: "c524cfcd8a6a811f40fb7f5dadfd671944da0c70d78befbec43f4b81a7b7cd47"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
             url: "https://github.com/hewigovens/wallet-core-spm/releases/download/0.0.1/SwiftProtobuf.xcframework.zip",
             checksum: "270a6545f72a512aafc7d7ecb73621005248d4ea44f7ebbc06a2f33c7d15bc4c"
-        ),
-        .binaryTarget(
-            name: "WalletCoreCommon",
-            url: "https://github.com/hewigovens/wallet-core-spm/releases/download/0.0.1/WalletCoreCommon.xcframework.zip",
-            checksum: "7e84ca6c02b9a54f5ff40ddf97f30497b4951a7036d599f16e2892f84945a72e"
-        ),
+        )
     ]
 )
